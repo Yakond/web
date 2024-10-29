f1e5653f91c3c2a895213fae967d059a8f86f49b
Yakond <186690518+Yakond@users.noreply.github.com>
2024-10-29 13:42:11 +0200

    commit

--- src/script.js
+++ src/script.js
@@ -1,14 +1,46 @@
-const params = new URLSearchParams(new URL(window.location.href).search);
-const filename = params.get("filename");
-// const control = document.getElementById("control")
-const view = document.getElementById("view")
-function useImage(filename, type) {
-    view.style.backgroundImage = `url(./assets/viewer/${filename}.${type})`
-}
-useImage(filename, "png")
-const buttons = {
-    png: document.getElementById("btn/picker/png"),
-    svg: document.getElementById("btn/picker/svg")
-}
-buttons.png.addEventListener("click", () => useImage(filename, "png"))
-buttons.svg.addEventListener("click", () => useImage(filename, "svg"))
+(async ()=>{
+    const params = new URLSearchParams(new URL(window.location.href).search);
+    const filename = params.get("filename");
+    let more_buttons = await fetch();
+
+    const control = document.getElementById("control")
+    const splitter = document.createElement("splitter")
+    if (more_buttons.length > 0) {
+        try {
+            splitter.setAttribute("id", "btn/splitter")
+            control.append(splitter)
+            let i = 0;
+            for (const {type, filename, label} of more_buttons) {
+                i++;
+                const button = document.createElement()
+                button.innerText = label;
+                switch (type) {
+                    case "file":
+                        button.setAttribute("id", "btn/file/"+i)
+                        button.addEventListener("click", () => showImage(filename, "png"));
+                        break;
+                    case "url":
+                        button.setAttribute("id", "btn/url/"+i)
+                        button.addEventListener("click", () => location.replace(button.url));
+                        break
+                    case _:
+                        continue;
+                }
+                control.append(button)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    const view = document.getElementById("view")
+    function showImage(filename, type) {
+        view.style.backgroundImage = `url(./assets/viewer/${filename}.${type})`
+    }
+    showImage(filename, "png")
+    const buttons = {
+        png: document.getElementById("btn/picker/png"),
+        svg: document.getElementById("btn/picker/svg")
+    }
+    buttons.png.addEventListener("click", () => showImage(filename, "png"))
+    buttons.svg.addEventListener("click", () => showImage(filename, "svg"))
+})()
