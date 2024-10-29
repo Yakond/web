3188a97cc2bcaeb9b70294e0ef50274692ff4510
Yakond <186690518+Yakond@users.noreply.github.com>
2024-10-29 13:06:56 +0200

    commit

--- src/script.js
+++ src/script.js
@@ -1,16 +1,14 @@
 const params = new URLSearchParams(new URL(window.location.href).search);
 const filename = params.get("filename");
-useImage(filename, "png")
 // const control = document.getElementById("control")
 const view = document.getElementById("view")
-
+function useImage(filename, type) {
+    view.style.backgroundImage = `url(./assets/${filename}.${type})`
+}
+useImage(filename, "png")
 const buttons = {
     png: document.getElementById("btn/png"),
     svg: document.getElementById("btn/svg")
-}
-function useImage(filename, type) {
-    view.style.backgroundImage = `url(./assets/${filename}.${type})`
-
 }
 buttons.png.addEventListener("click", () => useImage(filename, "png"))
 buttons.svg.addEventListener("click", () => useImage(filename, "svg"))
